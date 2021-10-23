28c1909149cda073e4fe5b1ff9dd1ecac375cad0
Darylste <daryl.stensland13@icloud.com>
2021-10-23 12:21:10 +0100

    shop page added and colletion components created

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import './App.css';
 import { Switch, Route } from 'react-router';
 
 import HomePage from './pages/homepage/homePage.component';
+import ShopPage from './pages/shopPage/shopPage.component';
 
-const HatsPage = () => (
-	<div>
-		<h1>HATS PAGE</h1>
-	</div>
-);
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route exact path='/' component={HomePage} />
-				<Route exact path='/hats' component={HatsPage} />
+				<Route exact path='/shop' component={ShopPage} />
 			</Switch>
 		</div>
 	);
