cb8e009e0bbcc7292f3d25d6729068cb0ab26226
Darylste <daryl.stensland13@icloud.com>
2021-10-23 11:17:16 +0100

    set-up router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 
+import { Switch, Route } from 'react-router';
+
 import HomePage from './pages/homepage/homePage.component';
 
+const HatsPage = () => (
+	<div>
+		<h1>HATS PAGE</h1>
+	</div>
+);
 function App() {
 	return (
-		<div className='App'>
-			<HomePage />
+		<div>
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route exact path='/hats' component={HatsPage} />
+			</Switch>
 		</div>
 	);
 }
